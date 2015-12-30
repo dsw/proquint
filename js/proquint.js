1980807b3dfdcaf4f7447dc00a857b2921dab5a4
l-ra <lra@r73.info>
2015-12-30 15:06:34 +0100

    proquint impl moved to separate file

--- js/proquint.js
+++ js/proquint.js
@@ -1,27 +1,8 @@
-/** GUI related stuff*/
-var inp=document.querySelector("#txtInput");
-var out=document.querySelector("#txtOutput");
-var btnConv=document.querySelector("#btnConvert");
-var btnSwap=document.querySelector("#btnSwap");
-var tt=document.querySelector("#toggle-theory");
-var tho=document.querySelector("#theory");
-
-btnConv.addEventListener("click", attemptConversion);
-btnSwap.addEventListener("click", function (){ var x=inp.value; inp.value=out.value; out.value=x;});
-tt.addEventListener("click", function (){
-	tho.classList.toggle("hidden");
-})
-
-function attemptConversion(){
-  console.log("attemptConversino");
-  if (inp.value.match(/^([bdfghjklmnprstvzaiou]{5}-?)+$/)) out.value=quint2hex(inp.value);
-  else if (inp.value.match(/^x(([0-9a-f]{2}){2})+$/)) out.value=hex2quint(inp.value);
-  else 
-     alert("Input format unsupported.");
-}
-
+/** 
+Converts quint list (dash separated) of arbitrary length to corresponding hex dump prefixed with x
+Quint list must contain pairs of quints to align with 32 bit boundary
+*/
 function quint2hex(inp){
-  console.log("quint2hex");
   var ret="x";
   var remaining=inp;
   while (remaining.length>0){
@@ -32,20 +13,20 @@ function quint2hex(inp){
     }
     if ( remaining.length>12 ) remaining=remaining.substring(12);
     else remaining=remaining.substring(11);
-    console.log(">>"+current+":"+remaining);
     var currentHex=quint2uint(current).toString(16);
-    console.log("currentHex:"+currentHex);
     while(currentHex.length<8) currentHex="0"+currentHex;
     ret+=currentHex;
   }
   return ret;
 }
 
-
+/** 
+Converts hex dump (x prefixed) to a list of quints. Hex dump must contain leading zeros when necessary.
+Hex dump length must by divisible by 8.
+*/
 function hex2quint(hex){
   var sep="-"; 
   var ret="";
-  console.log("hex2quint");
   if (! inp.value.match(/^x(([0-9a-f]{2}){2})+$/)) {
       console.err("Bad hex length: "+hex.length);
       throw ("Bad hex length: "+hex.length);
@@ -58,9 +39,6 @@ function hex2quint(hex){
     }
     var current=remaining.substring(0,8);
     remaining=remaining.substring(8);
-
-    console.log(">>"+current+":"+remaining);
-
     var currentQuint=uint2quint(parseInt(current,16)>>0);
     ret+=currentQuint;
     if ( sep ) ret+=sep;
@@ -70,21 +48,8 @@ function hex2quint(hex){
 }
 
 
+//following code is based on original java impl.
 
-function ipv62quint(){
-  console.log("ipv62quint");
-  
-
-}
-
-function ipv42quint(){
-  console.log("ipv42quint");
-
-}
-
-
-
-/** proquint implementation */
 var uint2consonant = [
     'b', 'd', 'f', 'g',
     'h', 'j', 'k', 'l',
@@ -119,7 +84,6 @@ function uint2quint(i) {
     j = i & MASK_FIRST4; i <<= 4; j >>>= 28; quint+=(uint2consonant[j]);
 	  return quint;
 }
-
 
 function quint2uint(quint) {
     var res = 0;
@@ -161,6 +125,6 @@ function quint2uint(quint) {
       default: break;
       }
     }
-    return res>>>0; //fix signed int issue - http://stackoverflow.com/a/17106974/1777150 
+    return res>>>0; //the unsigned right shift fixes signed int issue - http://stackoverflow.com/a/17106974/1777150 
 }
 
